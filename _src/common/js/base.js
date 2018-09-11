4285acc1daf4126169281e57e63b72e239293247
下坂　辰弥 <>
2018-09-12 01:12:49 +0900

    ◎スキルページにおいて、スクロール値に応じてskillNameSubの大きさを調整。

--- _src/common/js/base.js
+++ _src/common/js/base.js
@@ -9,3 +9,21 @@ $(function(){
       return false;
    });
 });
+
+// ハンバーガーメニューを開いたときにページのスクロールを禁止に
+$(function(){
+	var state = false;
+	var scrollpos;
+	
+	$('.btn-hamburgerMenu').on('click', function(){
+		if(state == false) {
+			scrollpos = $(window).scrollTop();
+			$('.area-content').addClass('area-content--inactive').css({'top': -scrollpos});
+			state = true;
+		} else {
+			$('.area-content').removeClass('area-content--inactive').css({'top': 0});
+			window.scrollTo( 0 , scrollpos );
+			state = false;
+		}
+	});
+});
