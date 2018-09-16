8164de63d4769e365b9c95d9489145cddda7b1eb
下坂　辰弥 <>
2018-09-17 03:16:26 +0900

    ◎各ページの基礎的なファイルを作成。 ◎このあとコンテンツを挿入していく。（topとUXについては対応済み。）

--- _src/top/js/top.js
+++ _src/top/js/top.js
@@ -3,9 +3,6 @@ $(window).load(function() {
 	var windowHeight = $(window).height(); //ウィンドウの高さを取得
 	var documentHeight = $(document).height(); //ページの高さを取得
 	var maxScrollVlue = documentHeight - windowHeight; //最大のスクロール値
-	var valueScrollGauge = scroll / (maxScrollVlue) * 100; //スクロール値の百分率
-	
-	$(".bx-scrollGauge").css({width: valueScrollGauge + "%"});
 	
 	$(".box-ScrollAnimaton").each(function(){
 		var position = $(this).offset().top; //ページの一番上から要素までの距離を取得
